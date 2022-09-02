a51d53ef2151a5920426c10187302edb428cbd24
saber nikfar <nikfar.saber@gmail.com>
2022-09-02 13:57:50 +0430

    write a function which takes an element type and a callback function.

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ function setterGenerator(inputProperty) {
 // const nameSetter = setterGenerator("name");
 // nameSetter.call(user, "jack");
 // console.log(user);
+
+//
+//
+/*
+Question2:
+2. write a function which takes an element type and a callback function.
+the function should create the corresponding element, append it to dom (wherever) then pass the reference to callback
+example:
+const callback = function(elem ){
+elem.style.backgroundColor = ‘red’ }
+elemCreator(‘div’ , callback)
+*/
+function elemCreator(element, callBackFunction) {
+  const newElement = document.createElement(element);
+  newElement.style.width = "100px";
+  newElement.style.height = "100px";
+  document.body.appendChild(newElement);
+  callBackFunction(newElement);
+}
+
+function callback(elem) {
+  elem.style.backgroundColor = "red";
+}
+// check answer
+// elemCreator("div", callback);
+
+//
+//
